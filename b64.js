3f6214a5ab280286a3ac662287e98ab5eff18bca
scott88lee <scott88lee@gmail.com>
2021-02-20 19:56:31 +0800

    Experimenting with B64

--- b64.js
+++ b64.js
@@ -5,7 +5,17 @@ const fs = require('fs');
 if (mode == 'e') {
     let buff = fs.readFileSync('raw');
     let base64data = buff.toString('base64');
-    fs.
+    fs.writeFile('data.b64', base64data, function (err) {
+        if (err) throw err;
+        console.log('Encoded!');
+      });
 } else if (mode == 'd') {
-    console.log('Decode')
+    let data = fs.readFileSync('data.b64');
+    console.log(data)
+    let bufferObj = Buffer.from(data, "base64");
+
+    // Encode the Buffer as a utf8 string
+    let decodedString = data.toString("utf8");
+    console.log(decodedString);
+    // console.log(data)
 }
