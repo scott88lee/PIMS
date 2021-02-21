ff05241122f95b9faebcae213695c4d3246a0eb1
scott88lee <scott88lee@gmail.com>
2021-02-21 17:03:27 +0800

    Encode and Decode success

--- b64.js
+++ b64.js
@@ -1,21 +1,36 @@
 let mode = process.argv[2]
 
 const fs = require('fs');
+const b64_file = "data.b64"
+const text_file = "text.raw"
 
 if (mode == 'e') {
-    let buff = fs.readFileSync('raw');
-    let base64data = buff.toString('base64');
-    fs.writeFile('data.b64', base64data, function (err) {
+    let str = fs.readFileSync(text_file);
+    let buff = Buffer.from(str);
+    let data = buff.toString('base64');
+    fs.writeFile(b64_file, data, function (err) {
+        if (err) throw err;
+            console.log('Encoded!');
+        });
+
+} else if (mode == 'd') {
+    let str = fs.readFileSync(b64_file).toString('utf8');
+
+    let buff = Buffer.from(str, 'base64');
+    let text = buff.toString('ascii');
+
+    console.log(text)
+    fs.writeFile(text_file, text, function (err) {
+        if (err) throw err;
+            console.log('Encoded!');
+        });
+
+} else {
+    let str = 'Hello World';
+    let buff = Buffer.from(str);
+    let data = buff.toString('base64');
+    fs.writeFile(b64_file, data, function (err) {
         if (err) throw err;
         console.log('Encoded!');
       });
-} else if (mode == 'd') {
-    let data = fs.readFileSync('data.b64');
-    console.log(data)
-    let bufferObj = Buffer.from(data, "base64");
-
-    // Encode the Buffer as a utf8 string
-    let decodedString = data.toString("utf8");
-    console.log(decodedString);
-    // console.log(data)
 }
